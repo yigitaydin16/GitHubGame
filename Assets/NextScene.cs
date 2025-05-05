e87a12226baa80b01f4dd21cc9940e23375ef306
Yigit Aydin <yigitay008@gmail.com>
2025-05-05 18:33:09 +0300

    Next Scene kodu ekledim

---
+++ Assets/NextScene.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+            else
+            {
+                Debug.Log("Son sahnedesin, geçilecek baþka sahne yok.");
+            }
+        }
+    }
+}
